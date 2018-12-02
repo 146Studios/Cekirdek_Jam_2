4542c7dd77367047a86aa55e61d9e75ca7b4873b
KillerJacKTM05 <34382594+KillerJacKTM05@users.noreply.github.com>
2018-12-02 13:40:38 +0300

    assets1

---
+++ Cekirdek_Jam_2-master/Cekirdek_Jam_2-master/Intermediate/Build/Win64/GameJamUntitledEditor/Development/Engine/SharedPCH.Engine.cpp
@@ -0,0 +1,1 @@
+#include "C:/Users/doguk/Desktop/Cekirdek_Jam_2-master/Cekirdek_Jam_2-master/Intermediate/Build/Win64/GameJamUntitledEditor/Development/Engine/SharedPCH.Engine.h"
---
+++ Cekirdek_Jam_2-master/Cekirdek_Jam_2-master/Intermediate/Build/Win64/UE4Editor/Inc/GameJamUntitled/GameJamUntitledGameModeBase.generated.h
@@ -0,0 +1,90 @@
+// Copyright 1998-2018 Epic Games, Inc. All Rights Reserved.
+/*===========================================================================
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===========================================================================*/
+
+#include "UObject/ObjectMacros.h"
+#include "UObject/ScriptMacros.h"
+
+PRAGMA_DISABLE_DEPRECATION_WARNINGS
+#ifdef GAMEJAMUNTITLED_GameJamUntitledGameModeBase_generated_h
+#error "GameJamUntitledGameModeBase.generated.h already included, missing '#pragma once' in GameJamUntitledGameModeBase.h"
+#endif
+#define GAMEJAMUNTITLED_GameJamUntitledGameModeBase_generated_h
+
+#define Cekirdek_Jam_2_master_Source_GameJamUntitled_GameJamUntitledGameModeBase_h_15_RPC_WRAPPERS
+#define Cekirdek_Jam_2_master_Source_GameJamUntitled_GameJamUntitledGameModeBase_h_15_RPC_WRAPPERS_NO_PURE_DECLS
+#define Cekirdek_Jam_2_master_Source_GameJamUntitled_GameJamUntitledGameModeBase_h_15_INCLASS_NO_PURE_DECLS \
+private: \
+	static void StaticRegisterNativesAGameJamUntitledGameModeBase(); \
+	friend struct Z_Construct_UClass_AGameJamUntitledGameModeBase_Statics; \
+public: \
+	DECLARE_CLASS(AGameJamUntitledGameModeBase, AGameModeBase, COMPILED_IN_FLAGS(0 | CLASS_Transient), CASTCLASS_None, TEXT("/Script/GameJamUntitled"), NO_API) \
+	DECLARE_SERIALIZER(AGameJamUntitledGameModeBase)
+
+
+#define Cekirdek_Jam_2_master_Source_GameJamUntitled_GameJamUntitledGameModeBase_h_15_INCLASS \
+private: \
+	static void StaticRegisterNativesAGameJamUntitledGameModeBase(); \
+	friend struct Z_Construct_UClass_AGameJamUntitledGameModeBase_Statics; \
+public: \
+	DECLARE_CLASS(AGameJamUntitledGameModeBase, AGameModeBase, COMPILED_IN_FLAGS(0 | CLASS_Transient), CASTCLASS_None, TEXT("/Script/GameJamUntitled"), NO_API) \
+	DECLARE_SERIALIZER(AGameJamUntitledGameModeBase)
+
+
+#define Cekirdek_Jam_2_master_Source_GameJamUntitled_GameJamUntitledGameModeBase_h_15_STANDARD_CONSTRUCTORS \
+	/** Standard constructor, called after all reflected properties have been initialized */ \
+	NO_API AGameJamUntitledGameModeBase(const FObjectInitializer& ObjectInitializer = FObjectInitializer::Get()); \
+	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(AGameJamUntitledGameModeBase) \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, AGameJamUntitledGameModeBase); \
+DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(AGameJamUntitledGameModeBase); \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API AGameJamUntitledGameModeBase(AGameJamUntitledGameModeBase&&); \
+	NO_API AGameJamUntitledGameModeBase(const AGameJamUntitledGameModeBase&); \
+public:
+
+
+#define Cekirdek_Jam_2_master_Source_GameJamUntitled_GameJamUntitledGameModeBase_h_15_ENHANCED_CONSTRUCTORS \
+	/** Standard constructor, called after all reflected properties have been initialized */ \
+	NO_API AGameJamUntitledGameModeBase(const FObjectInitializer& ObjectInitializer = FObjectInitializer::Get()) : Super(ObjectInitializer) { }; \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API AGameJamUntitledGameModeBase(AGameJamUntitledGameModeBase&&); \
+	NO_API AGameJamUntitledGameModeBase(const AGameJamUntitledGameModeBase&); \
+public: \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, AGameJamUntitledGameModeBase); \
+DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(AGameJamUntitledGameModeBase); \
+	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(AGameJamUntitledGameModeBase)
+
+
+#define Cekirdek_Jam_2_master_Source_GameJamUntitled_GameJamUntitledGameModeBase_h_15_PRIVATE_PROPERTY_OFFSET
+#define Cekirdek_Jam_2_master_Source_GameJamUntitled_GameJamUntitledGameModeBase_h_12_PROLOG
+#define Cekirdek_Jam_2_master_Source_GameJamUntitled_GameJamUntitledGameModeBase_h_15_GENERATED_BODY_LEGACY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	Cekirdek_Jam_2_master_Source_GameJamUntitled_GameJamUntitledGameModeBase_h_15_PRIVATE_PROPERTY_OFFSET \
+	Cekirdek_Jam_2_master_Source_GameJamUntitled_GameJamUntitledGameModeBase_h_15_RPC_WRAPPERS \
+	Cekirdek_Jam_2_master_Source_GameJamUntitled_GameJamUntitledGameModeBase_h_15_INCLASS \
+	Cekirdek_Jam_2_master_Source_GameJamUntitled_GameJamUntitledGameModeBase_h_15_STANDARD_CONSTRUCTORS \
+public: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+#define Cekirdek_Jam_2_master_Source_GameJamUntitled_GameJamUntitledGameModeBase_h_15_GENERATED_BODY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	Cekirdek_Jam_2_master_Source_GameJamUntitled_GameJamUntitledGameModeBase_h_15_PRIVATE_PROPERTY_OFFSET \
+	Cekirdek_Jam_2_master_Source_GameJamUntitled_GameJamUntitledGameModeBase_h_15_RPC_WRAPPERS_NO_PURE_DECLS \
+	Cekirdek_Jam_2_master_Source_GameJamUntitled_GameJamUntitledGameModeBase_h_15_INCLASS_NO_PURE_DECLS \
+	Cekirdek_Jam_2_master_Source_GameJamUntitled_GameJamUntitledGameModeBase_h_15_ENHANCED_CONSTRUCTORS \
+private: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+#undef CURRENT_FILE_ID
+#define CURRENT_FILE_ID Cekirdek_Jam_2_master_Source_GameJamUntitled_GameJamUntitledGameModeBase_h
+
+
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
